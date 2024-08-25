51cbb729ff763ff58c8b3d231fe2b4954a2e781a
hikizutte <mousi11128@gmail.com>
2024-08-25 18:21:13 +0900

    simpleBowItemメソッドの作成完了。datagenの一区切りの完成。

--- src/main/java/io/Kmod/patchworkmod/datagen/ModItemModelProvider.java
+++ src/main/java/io/Kmod/patchworkmod/datagen/ModItemModelProvider.java
@@ -1,13 +1,13 @@
 package io.Kmod.patchworkmod.datagen;
 
 import io.Kmod.patchworkmod.main.Patchworkmod;
-import io.Kmod.patchworkmod.regi.PatchworkModBlocks;
 import io.Kmod.patchworkmod.regi.PatchworkModItems;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -29,7 +29,7 @@ public class ModItemModelProvider extends ItemModelProvider {
         simpleItem(PatchworkModItems.OBSIDIAN_LEGGINGS);
         simpleItem(PatchworkModItems.OBSIDIAN_BOOTS);
 
-        simpleItem(PatchworkModItems.OBSIDIAN_BOW);
+        simpleBowItem(PatchworkModItems.OBSIDIAN_BOW);
 
     }
 
@@ -39,12 +39,39 @@ public class ModItemModelProvider extends ItemModelProvider {
                 new ResourceLocation(Patchworkmod.MODID,"item/" + item.getId().getPath()));
     }
 
-    private ItemModelBuilder.OverrideBuilder simpleBowItem(RegistryObject<Item> item) {
-        return withExistingParent(item.getId().getPath(),
-                new ResourceLocation("item/bow")).texture("layer0",
-                        new ResourceLocation(Patchworkmod.MODID,"item/" + item.getId().getPath())).override()
-                .predicate(new ResourceLocation(Patchworkmod.MODID, "item/" + item.getId().getPath() + "_pulling_0"), 0)
-                .predicate(new ResourceLocation(Patchworkmod.MODID, "item/" + item.getId().getPath() + "_pulling_1"), 1)
-                .predicate(new ResourceLocation(Patchworkmod.MODID, "item/" + item.getId().getPath() + "_pulling_2"), 2);
+    private void simpleBowItem(RegistryObject<Item> item) {
+        // ベースの弓アイテムのモデルを設定
+        getBuilder(item.getId().getPath())
+                .parent(new ModelFile.UncheckedModelFile("item/bow"))
+                .texture("layer0", new ResourceLocation(Patchworkmod.MODID, "item/" + item.getId().getPath()))
+                .override()
+                .predicate(new ResourceLocation("pulling"), 1)
+                .model(new ModelFile.UncheckedModelFile(new ResourceLocation(Patchworkmod.MODID, "item/" + item.getId().getPath() + "_pulling_0")))
+                .end()
+                .override()
+                .predicate(new ResourceLocation("pulling"), 1)
+                .predicate(new ResourceLocation("pull"), 0.65f)
+                .model(new ModelFile.UncheckedModelFile(new ResourceLocation(Patchworkmod.MODID, "item/" + item.getId().getPath() + "_pulling_1")))
+                .end()
+                .override()
+                .predicate(new ResourceLocation("pulling"), 1)
+                .predicate(new ResourceLocation("pull"), 0.9f)
+                .model(new ModelFile.UncheckedModelFile(new ResourceLocation(Patchworkmod.MODID, "item/" + item.getId().getPath() + "_pulling_2")))
+                .end();
+
+        // obsidian_bow_pulling_0.json の生成
+        getBuilder(item.getId().getPath() + "_pulling_0")
+                .parent(new ModelFile.UncheckedModelFile("item/generated"))
+                .texture("layer0", new ResourceLocation(Patchworkmod.MODID, "item/" + item.getId().getPath() + "_pulling_0"));
+
+        // obsidian_bow_pulling_1.json の生成
+        getBuilder(item.getId().getPath() + "_pulling_1")
+                .parent(new ModelFile.UncheckedModelFile("item/generated"))
+                .texture("layer0", new ResourceLocation(Patchworkmod.MODID, "item/" + item.getId().getPath() + "_pulling_1"));
+
+        // obsidian_bow_pulling_2.json の生成
+        getBuilder(item.getId().getPath() + "_pulling_2")
+                .parent(new ModelFile.UncheckedModelFile("item/generated"))
+                .texture("layer0", new ResourceLocation(Patchworkmod.MODID, "item/" + item.getId().getPath() + "_pulling_2"));
     }
 }
